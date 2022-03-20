472329a339b1e4ab1d3bc2dde971899656ccb35d
Keks <keks@htmlacademy.ru>
2022-03-20 15:34:39 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,6 @@
+let getRandomNumber = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let checkComment = function(comment, maxLenght) {
+    return comment.length <= maxLenght ? true : false
+}
