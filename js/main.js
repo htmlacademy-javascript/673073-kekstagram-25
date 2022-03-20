c9dbca94060bc72c55d6f56431d0dbc09852bdc4
denissvetlichnii <svetlichnii@gmail.com>
2022-03-20 14:31:45 +0200

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-let checkComment = function(comment,maxLenght) {
-    if(comment.length <= maxLenght) {
-        return true;
-    } else {
-        return false;
-    }
-
+let getRandomNumber = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let checkComment = function(comment, maxLenght) {
+    return comment.length <= maxLenght ? true : false
 }
