9c9ec321303cc540cead54e7a2d4d29103045cc6
Keks <keks@htmlacademy.ru>
2022-03-20 22:27:28 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,6 +1,8 @@
-let getRandomNumber = function(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-let checkComment = function(comment, maxLenght) {
-    return comment.length <= maxLenght ? true : false
-}
+// eslint-disable-next-line no-unused-vars
+const getRandomNumber = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+// eslint-disable-next-line no-unused-vars
+const checkComment = function(comment, maxLenght) {
+  return comment.length <= maxLenght;
+};
